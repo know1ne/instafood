f6afb150942803362aa79557a1b5b57c43bc66b5
Thoukeer <thoukeer86@gmail.com>
2024-03-16 06:36:44 +0530

    updated routes for specific changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
-import Parent from "./components/Parent/Parent";
+import Parent from "./components/Parent/Parent"; // Assuming this is the correct import path
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/orderingoptions" element={<Parent />} />
+          <Route path="/order" element={<Parent />} />
+          <Route path="/navbar" element={<Navbar />} /> {/* Corrected path */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
